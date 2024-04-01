c63d8f1565950ca159c1aa4a89020bd855e7d75b
Zied <zied.kannou15@gmail.com>
2024-04-01 14:39:51 +0200

    project init

---
+++ src/main/java/com/authenticationApp/authenticationback/AuthenticationBackApplication.java
@@ -0,0 +1,13 @@
+package com.authenticationApp.authenticationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authenticationApp/authenticationback/AuthenticationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authenticationApp.authenticationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
